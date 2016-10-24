06699d2a957879201bbf52f1b9393d486e96110b
D <dylens.yang@derbysoft.com>
2016-10-24 19:47:48 +0800

    ..

--- src/main/java/fory/gson/JsonToJava.java
+++ src/main/java/fory/gson/JsonToJava.java
@@ -5,13 +5,15 @@ import com.google.gson.GsonBuilder;
 import fory.gson.module.Person;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class JsonToJava {
 
     public static void main(String[] args) throws IOException {
-        try(Reader reader = new InputStreamReader(JsonToJava.class.getResourceAsStream("/server.json"), "UTF-8")){
+        InputStream resourceAsStream = JsonToJava.class.getResourceAsStream("/server.json");
+        try(Reader reader = new InputStreamReader(resourceAsStream, "UTF-8")){
             Gson gson = new GsonBuilder().create();
             Person p = gson.fromJson(reader, Person.class);
             System.out.println(p);
