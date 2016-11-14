a92182830195b196f8ab57d7219835a04a0e4514
D <dylens.yang@derbysoft.com>
2016-11-14 17:17:18 +0800

    ..

---
+++ src/main/java/fory/httpmock/DyeHttp.java
@@ -0,0 +1,35 @@
+package fory.httpmock;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DyeHttp {
+
+    public void start() throws IOException {
+        String url = "http://localhost:8090/dye";
+
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpGet request = new HttpGet(url);
+        request.setHeader(new BasicHeader("Accept","text/xml"));
+
+        HttpResponse response = client.execute(request);
+        System.out.println("Response Code: " +
+                response.getStatusLine().getStatusCode());
+
+        BufferedReader rd = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+        String line;
+        while((line = rd.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+
+}
---
+++ src/test/java/fory/httpmock/DyeHttpTest.java
@@ -0,0 +1,37 @@
+package fory.httpmock;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.Before;
+import org.junit.Test;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+
+
+public class DyeHttpTest {
+
+    WireMockServer wireMockServer;
+
+    @Before
+    public void before() {
+        wireMockServer = new WireMockServer(wireMockConfig().port(8090));
+        WireMock.configureFor("localhost", 8090);
+        wireMockServer.start();
+
+        stubFor(get(urlEqualTo("/dye"))
+                .withHeader("Accept", equalTo("text/xml"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "text/xml")
+                        .withBody("Hello world!")));
+    }
+
+    @Test
+    public void testStart() throws Exception {
+        DyeHttp client = new DyeHttp();
+        client.start();
+
+        wireMockServer.stop();
+    }
+}
