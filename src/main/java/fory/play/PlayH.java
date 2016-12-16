1af9c3e7e0c260d51cebf41516ce81971662d3b4
D <dylens.yang@derbysoft.com>
2016-12-16 17:08:11 +0800

    ..

--- src/main/java/fory/play/PlayH.java
+++ src/main/java/fory/play/PlayH.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class PlayH {
 
     public static void main(String[] args) {
-        //四舍五入，逢5奇进偶舍
+        //四舍六入，逢5奇进偶舍
         System.out.println(BigDecimal.valueOf(1.115).setScale(2, BigDecimal.ROUND_HALF_EVEN));//1.12
         System.out.println(BigDecimal.valueOf(1.125).setScale(2, BigDecimal.ROUND_HALF_EVEN));//1.12
         System.out.println(BigDecimal.valueOf(1.135).setScale(2, BigDecimal.ROUND_HALF_EVEN));//1.14
